8b86b13133a5d31ceaf19cb679e4130b0ad75bad
solomon-winner <solyalew3@gmail.com>
2023-12-19 10:24:14 +0300

    progressing...

--- api/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {login, register, logout} from "../controllers/auth.js"
-const router = express.Router();
-
-router.post("/login", login);
-router.post("/register", register);
-router.post("/logout", logout);
-
-export default router;
--- api/routes/users.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {getUser,updateUser} from "../controllers/user.js"
-const router = express.Router();
-
-router.get("/find/:userId", getUser);
-router.put("/", updateUser);
-
-export default router;
